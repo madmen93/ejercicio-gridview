f95128d58a8dd1dfc380867480e3fcc946eb352c
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-07-19 18:15:39 -0500

    Se creó clases Direccion y DireccionNegocio y el método que devuelve la lista

---
+++ gridviewejercicio/dominio/Direccion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Direccion
+    {
+        public string calle { get; set; }
+        public int numero { get; set; }
+        public int codigoPostal { get; set; }
+        public string distrito { get; set; }
+    }
+    public class DireccionNegocio
+    {
+        public List<Direccion> listar()
+        {
+            List<Direccion> lista = new List<Direccion>();
+            lista.Add(new Direccion());
+            lista.Add(new Direccion());
+            lista.Add(new Direccion());
+
+            lista[0].calle = "Jr Rousseau";
+            lista[0].numero = 220;
+            lista[0].codigoPostal = 15037;
+            lista[0].distrito = "San Borja";
+            
+            lista[1].calle = "Av México";
+            lista[1].numero = 1150;
+            lista[1].codigoPostal = 15000;
+            lista[1].distrito = "La Victoria";
+            
+            lista[2].calle = "Jr Luna Pizarro";
+            lista[2].numero = 235;
+            lista[2].codigoPostal = 15018;
+            lista[2].distrito = "La Victoria";
+            
+            return lista;
+        }
+    }
+}
